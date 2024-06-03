0a3c05122945f655cf8fe79059b214993c125dbe
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-06-03 12:05:32 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/ArrowDraw.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowDraw : MonoBehaviour
+{
+    [SerializeField]
+    private Image arrowImage;
+    private Vector3 clickPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrowImage.gameObject.SetActive(false);
+
+        //クリックしたタイミングでtrueを返す
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+        }
+
+        //クリックしているとtrueを返す
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 dist = clickPosition - Input.mousePosition;
+            //ベクトルの長さを算出
+            float size = dist.magnitude;
+            //ベクトルから角度(弧度法)を算出
+            float angleRad = Mathf.Atan2(dist.y, dist.x);
+            //矢印の画像をクリックした場所に画像を移動
+            arrowImage.rectTransform.position = clickPosition;
+            //矢印の画像をベクトルから算出した角度を度数法に変換してZ軸回転
+            arrowImage.rectTransform.rotation = Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
+            //矢印の画像の大きさをドラッグした距離に合わせる
+            arrowImage.rectTransform.sizeDelta = new Vector2(size, size);
+            Debug.Log(dist);
+            //クリックしたら矢印を描画
+            arrowImage.gameObject.SetActive(true);
+        }
+    }
+}
